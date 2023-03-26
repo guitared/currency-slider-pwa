de890fd7c40a56c93ab69132fac6295b74e0ea5e
Guitared <guitared@users.noreply.github.com>
2023-03-26 18:29:52 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,13 +8,13 @@ var APP_PREFIX = 'cspwa_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_00';
+var VERSION = 'version_01';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
-  `${GHPATH}/css/styles.css`,
-  `${GHPATH}/js/app.js`
+  `${GHPATH}/styles.css`,
+  `${GHPATH}/app.js`
 ]
